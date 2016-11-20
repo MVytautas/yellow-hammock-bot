11f6436e00ae2ab296fb6f186b5f772dbac1f2e5
Martynas Drobulis <mdrobulis@gmail.com>
2016-11-21 01:48:57 +0200

    clj-http requests to fb graph api

--- project.clj
+++ project.clj
@@ -12,6 +12,8 @@
                  [ring "1.5.0"]
                  [ring/ring-defaults "0.2.1"]
                  [compojure "1.5.1"]
+                 [clj-http "2.3.0"]
+                 [org.clojure/data.json "0.2.6"]
                  [hiccup "1.0.5"]
                  [yogthos/config "0.8"]
                  [org.clojure/clojurescript "1.9.229"
--- src/clj/yellow_hammock/handler.clj
+++ src/clj/yellow_hammock/handler.clj
@@ -1,9 +1,12 @@
 (ns yellow-hammock.handler
-  (:require [compojure.core :refer [GET defroutes]]
+  (:require [compojure.core :refer :all ]
             [compojure.route :refer [not-found resources]]
             [hiccup.page :refer [include-js include-css html5]]
             [yellow-hammock.middleware :refer [wrap-middleware]]
-            [config.core :refer [env]]))
+            [config.core :refer [env]]
+            [clj-http.client :as client]
+            [clojure.data.json :as json]
+            ))
 
 (def mount-target
   [:div#app
@@ -26,12 +29,31 @@
      mount-target
      (include-js "/js/app.js")]))
 
+(def fb-messages "https://graph.facebook.com/v2.6/me/messages")
+(def fb-convo "https://graph.facebook.com/v2.6/me/conversations")
+(def qs "EAACN3fv7nN0BAO1qGPq12NswFtP8L9RWN3YE9IFl1K9wvAQDla4THwqg7Bpwh73ebM8MSvnfJMfZAvPZBTrWroalp7ZACZBcw5DSS1xFU2GDqHIMKpb1K9wkoMMfmfFFntzNfmrwuA33Vu45UQq2vuoqyzKsWSINfrFsksbtfAZDZD")
 
-(defroutes routes
+(defn get-convo [] (client/get  (str fb-convo "?access_token=" qs) {
+                                                                    :throw-exceptions false}))
+
+((get-convo) :body)
+
+
+
+
+(defroutes my_routes
   (GET "/" [] (loading-page))
-  (GET "/about" [] (loading-page))
-  
+  (GET "/fb/webhook"  request (println str request))
+  (POST  "/fb/webhook" request (str request ))
+
   (resources "/")
   (not-found "Not Found"))
 
-(def app (wrap-middleware #'routes))
+(def app (wrap-middleware #'my_routes))
+
+
+
+
+
+
+
